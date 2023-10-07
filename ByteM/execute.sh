e3e1d73e5fa11e4f3b3964c3e36da3d1ee58225e
tniromin <tniromin2@gmail.com>
2023-10-07 11:42:45 +0530

    Update execute.sh

--- ByteM/execute.sh
+++ ByteM/execute.sh
@@ -1,3 +1,14 @@
 # A random count of files will be generated within the src directory once the tests are run.
 
 # Write your code here
+# A random count of files will be generated within the src directory once the tests are run.
+for file in src/*
+do
+    if [ -f "$file" ] # Check if it's a regular file
+    then
+        size=$(wc -c < "$file")
+        totalSize=$((totalSize + size))
+    fi
+done
+
+echo "$totalSize"
